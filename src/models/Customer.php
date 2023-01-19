9fe9568b15e79e48900da93179b99b90d1c9be24
jaum1234 <joaovitorsouzacoura@gmail.com>
2023-01-19 11:45:30 -0300

    :sparkles: feat: Customer entity.

---
+++ src/models/Customer.php
@@ -0,0 +1,35 @@
+<?php
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: "customers")]
+class Customer
+{
+    #[ORM\Id]
+    #[ORM\Column(type: "integer")]
+    #[ORM\GeneratedValue()]
+    private int $id;
+
+    #[ORM\Column(type: "string", nullable: false)]
+    private string $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
+
+?>
