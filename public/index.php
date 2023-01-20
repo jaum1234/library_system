0d686ceb2f42115551a6724d6ccb1d957eaadc4f
jaum1234 <joaovitorsouzacoura@gmail.com>
2023-01-19 21:51:57 -0300

    :recycle: refactor: routes.

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@ $ids = $request->ids();
 $firstResource = $resources[0];
 $secondResource = $resources[1] != null ? "/$resources[1]" : "";
 
-$firstParameter = $ids[0] != 0 ? "/$ids[0]" : "";
+$firstParameter = $ids[0] != 0 ? "/{$ids[0]}" : "";
 $secondParameter = $ids[1] != 0 ? "/$ids[1]" : "";
 
 $action = $routes["$method|/$resources[0]" . $firstParameter . $secondResource . $secondParameter];
--- routes/routes.php
+++ routes/routes.php
@@ -2,27 +2,37 @@
 
 use Library\Controllers\AuthorsController;
 use Library\Controllers\BooksController;
+use Library\Helpers\Request;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-$uri = $_SERVER["REQUEST_URI"];
+$request = new Request();
 
-$id = explode("/", $uri)[2];
+$id = $request->ids()[0];
 
-return [
+$booksRoutes = [
     "GET|/books" => [BooksController::class, "list"],
     "POST|/books" => [BooksController::class, "create"],
     "GET|/books/$id" => [BooksController::class, "show"],
     "DELETE|/books/$id" => [BooksController::class, "destroy"],
     "PUT|/books/$id" => [BooksController::class, "update"],
-    "GET|/books/$id/authors" => [BooksController::class, "listBookAuthors"],
+    "GET|/books/$id/authors" => [BooksController::class, "listBookAuthors"]
+];
+
+$authorsRoutes = [
     "POST|/authors" => [AuthorsController::class, "create"],
     "GET|/authors" => [AuthorsController::class, "list"],
     "GET|/authors/$id" => [AuthorsController::class, "show"],
     "PUT|/authors/$id" => [AuthorsController::class, "update"],
     "DELETE|/authors/$id" => [AuthorsController::class, "destroy"],
-    "GET|/authors/$id/books" => [AuthorsController::class, "listAuthorBooks"],
+    "GET|/authors/$id/books" => [AuthorsController::class, "listAuthorBooks"]
+];
+
+$customersRoutes = [
     "GET|/customers" => []
 ];
+
+return array_merge($booksRoutes, $authorsRoutes, $customersRoutes);
     
+?>
 
